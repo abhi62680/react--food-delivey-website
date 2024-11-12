9d598591f0bf81df91ce2e18a6445fd1cdb40f2c
abhi62680 <abhishekraj62680@gmail.com>
2024-11-12 20:35:55 +0530

    food

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
+
+const Footer = () => {
+  return (
+    <footer className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi voluptatum ullam aut veritatis cumque, in soluta vel praesentium doloremque enim, voluptas, velit temporibus ipsam! Dolorum aperiam minima dicta dolor voluptate!</p>
+          <div className="footer-social-icon">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-415-2560</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 Tomato.com - All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>order your favourite food here </h2>
+        <p>Choose from a diverse menu featuring a detactable array of dishes crafted with the finest ingredient and satisfy your craving and elevate your dining experince ,one delicious meal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
